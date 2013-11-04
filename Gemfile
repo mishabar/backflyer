4fbf0a49366bbfa0f61c398d193fd73bb19a18ba
Michael Bar <barmic@gmail.com>
2013-11-04 13:51:15 -0200

    New page UI

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'rails', '3.2.11'
 #gem 'sqlite3'
 
 gem 'httparty'
+gem 'mechanize'
 #gem 'pg'
 
 # Gems used only for assets and not required
--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -1,3 +1,5 @@
+require 'mechanize'
+require 'httparty'
 require 'json'
 
 class LinksController < ApplicationController
@@ -7,5 +9,33 @@ class LinksController < ApplicationController
 
   def ios
     render :text => "_bfShowLinks(#{LinksLoader.ios_links.to_json})"
+  end
+
+  def get_google_store_data
+    #  Get page details
+    mechanize = Mechanize.new { |agent|
+      agent.user_agent = request.env['HTTP_USER_AGENT']
+      agent.request_headers = { 'Accept-Language' => request.env['HTTP_ACCEPT_LANGUAGE'] }
+      agent.ssl_version = 'SSLv3'
+      agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
+    }
+
+    mechanize.get(params[:link]) do |page|
+      @name = page.at('div[@class="document-title"]').text
+      @image = page.at('img[@class="cover-image"]')[:src]
+    end
+
+    render :text => "_bfShowLink(#{ { :name => @name, :image => @image }.to_json })"
+  end
+
+  def get_itunes_data
+    app_id = /id\d+/.match(params['link'])
+    response = HTTParty.get("https://itunes.apple.com/lookup?id=#{app_id[0][2, app_id[0].length - 2]}")
+
+    render :text => "_bfShowLink(#{ { :name => response['results'][0]['trackName'], :image => response['results'][0]['artworkUrl100'] }.to_json })"
+  end
+
+  def new_page
+
   end
 end
