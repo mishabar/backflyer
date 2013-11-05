4b3531c41726fbe7e9d361e6b297b069304ca696
Michael Bar <barmic@gmail.com>
2013-11-05 14:47:53 -0200

    Basic API integration

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -36,15 +36,14 @@ class LinksController < ApplicationController
   end
 
   def new_page
-    #puts URI::escape("http://ads.appia.com/v2/getAds?id=571&password=XXGZVOSEURVRRQSNNQPV9XIJ1F&siteId=4140&categoryId=5,7,9,17,19,21,24&adTypeId=6&totalCampaignsRequested=5&sessionId=#{request.session_options[:id]}&userAgentHeader=#{request.env['HTTP_USER_AGENT']}&ipAddress=#{request.env['REMOTE_ADDR']}")
-    #request.env.each { |k,v| puts "#{k} - #{v}"}
     ipAddress = request.env['REMOTE_ADDR']
     unless request.env['HTTP_X_FORWARDED_FOR'] == nil
       ipAddress = request.env['HTTP_X_FORWARDED_FOR']
     end
 
     xml = HTTParty.get(URI::escape("http://ads.appia.com/v2/getAds?id=571&password=XXGZVOSEURVRRQSNNQPV9XIJ1F&siteId=4140&categoryId=5,7,9,17,19,21,24&adTypeId=6&totalCampaignsRequested=5&sessionId=#{request.session_options[:id]}&userAgentHeader=#{request.env['HTTP_USER_AGENT']}&ipAddress=#{ipAddress}"))
-    ##{request.env['REMOTE_ADDR']}
+    URI::escape("http://ads.appia.com/v2/getAds?id=571&password=XXGZVOSEURVRRQSNNQPV9XIJ1F&siteId=4140&categoryId=5,7,9,17,19,21,24&adTypeId=6&totalCampaignsRequested=5&sessionId=#{request.session_options[:id]}&userAgentHeader=#{request.env['HTTP_USER_AGENT']}&ipAddress=#{ipAddress}")
+    request.env.each { |k,v| puts "#{k} - #{v}"}
     @ads = xml['ads']['ad']
   end
 end
